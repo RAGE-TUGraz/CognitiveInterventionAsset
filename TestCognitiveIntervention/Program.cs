878ddb5b1e73b870e9f772227a0bb7668e392b0b
mmaurer <mmaurer@192.168.222.168>
2016-02-08 11:32:59 +0100

    created infrastructure (+ portable + path fix)

---
+++ TestCognitiveIntervention/Program.cs
@@ -0,0 +1,19 @@
+﻿using AssetPackage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCognitiveIntervention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CognitiveInterventionAsset cia = new CognitiveInterventionAsset();
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
